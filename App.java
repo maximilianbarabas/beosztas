374f600175c78fb33e04cf3d3d5f846ab212adf7
maximilianbarabas <93761052+maximilianbarabas@users.noreply.github.com>
2022-06-08 10:50:32 +0200

    Add files via upload

---
+++ App.java
@@ -0,0 +1,18 @@
+/*
+* File: MainController.java
+* Author: Barabas Maximilian
+* Copyright: 2022, Barabas Maximilian
+* Group: Szoft I N 
+* Date: 2022-05-31
+* Github: https://github.com/maximilianbarabas
+* Licenc: GNU GPL
+*/
+
+import controllers.MainController;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        new MainController();
+    }
+}
+
